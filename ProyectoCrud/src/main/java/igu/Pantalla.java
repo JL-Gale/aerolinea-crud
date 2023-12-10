fd297fce5a99c064feef666fce762eecc2ad3ffa
JL-Gale <86643760+JL-Gale@users.noreply.github.com>
2023-12-10 16:03:25 -0500

    Se agregaron cambios en el codigo de la igu

--- ProyectoCrud/src/main/java/igu/Pantalla.java
+++ ProyectoCrud/src/main/java/igu/Pantalla.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
- */
+
 package igu;
 
-/**
- *
- * @author SERGIO MARTINEZ
- */
 public class Pantalla extends javax.swing.JFrame {
 
     /**
